eb51e47a4bd50e34b759926611b5bf73ef439bd9
oomar1580 <mikobot94@gmail.com>
2025-09-27 18:43:12 +0200

    Update bank.jsn

--- modules/commands/bank.js
+++ modules/commands/bank.js
@@ -49,44 +49,44 @@ module.exports.run = async function({ api, event, args, models, Users, Threads, 
   else return api.sendMessage(`[ بنك اكامي ] » لديك بالفعل حساب على نظام البنك 🏦`, threadID, messageID)
   }
   if(args[0] == 'check' || args[0] == 'عرض') {
-  if (!user.find(i => i.senderID == senderID)) return api.sendMessage('[ بنك اكامي ] » انت مل مسجل بالبنك سجل وتعال 🏦', threadID, messageID)
+  if (!user.find(i => i.senderID == senderID)) return api.sendMessage('[بنك ميكو] » انت مل مسجل بالبنك سجل وتعال 🏦', threadID, messageID)
     else { 
       var userData = user.find(i => i.senderID == senderID);
       return api.sendMessage(`[ بنك اكامي ] » المبلغ الذي تودع به بنك اكامي هو: ${userData.money}$\n\n💷 الفائده: +${laisuat}% كل ${timeIM/60} دقيقة`, threadID, messageID)
     }
   } 
   if(args[0] == 'ايداع' || args[0] == 'send') {
-  if (!args[1] || isNaN(args[1]) || parseInt(args[1]) < 50) return api.sendMessage("[ بنك اكامي ] »يجب أن يكون مبلغ الإيداع رقمًا واحدًا وأكبر من 50 دولارًا 💰", threadID, messageID);
+  if (!args[1] || isNaN(args[1]) || parseInt(args[1]) < 50) return api.sendMessage("[ بنك ميكو] »يجب أن يكون مبلغ الإيداع رقمًا واحدًا وأكبر من 50 دولارًا 💰", threadID, messageID);
   if (!user.find(i => i.senderID == senderID)) {
     return api.sendMessage('[ بنك اكامي ] »  اكتب (بنك تسجيل) حتى تسجل بالبنك وتعرض فلوسك + نسبة الفئدة 💰', threadID, messageID)
   }
   else { 
       let balance = (await Currencies.getData(senderID)).money;
-      if(balance < moneyInput) return api.sendMessage(`[ بنك اكامي ] » رصيدك غير كاف ${moneyInput} للإيداع في بنك اكامي 💰 `, threadID, messageID)
+      if(balance < moneyInput) return api.sendMessage(`[ بنك ميكو] » رصيدك غير كاف ${moneyInput} للإيداع في بنك اكامي 💰 `, threadID, messageID)
       var userData = user.find(i => i.senderID == senderID);
       var money = userData.money;
       userData.money = parseInt(money) + parseInt(moneyInput)
       writeFileSync(pathData, JSON.stringify(user, null, 2));
       await Currencies.decreaseMoney(senderID, parseInt(moneyInput));
-      return api.sendMessage(`[ بنك اكامي ] » لقد قمت بأيداع  ${moneyInput}$ في بنك اكامي\n\n💷 الفائده: +${laisuat}% في ${timeIM/60} دقيقة`, threadID, messageID)
+      return api.sendMessage(`[ بنك اكامي ] » لقد قمت بأيداع  ${moneyInput}$ في بنك ميكو\n\\💷 الفائده: +${laisuat}% في ${timeIM/60} دقيقة`, threadID, messageID)
     }
   }
   if(args[0] == 'rút' || args[0] == 'سحب') { 
-    if (!args[1] || isNaN(args[1]) || parseInt(args[1]) < 50) return api.sendMessage("[ بنك اكامي ] » لازم تكتب رقم واكثر من 50 دولار . ", threadID, messageID);
+    if (!args[1] || isNaN(args[1]) || parseInt(args[1]) < 50) return api.sendMessage("[ بنك ميكو] » لازم تكتب رقم واكثر من 50 دولار . ", threadID, messageID);
     if (!user.find(i => i.senderID == senderID)) {
-      return api.sendMessage('[ بنك اكامي ] »  اكتب (بنك تسجيل) حتى تسجل بالبنك وتعرض فلوسك + نسبة الفئدة 💰', threadID, messageID)
+      return api.sendMessage('[ بنك ميكو] »  اكتب (بنك تسجيل) حتى تسجل بالبنك وتعرض فلوسك + نسبة الفئدة 💰', threadID, messageID)
     }
   else {  
     var userData = user.find(i => i.senderID == senderID); 
     var money = userData.money;
-    if(parseInt(money) < parseInt(moneyInput)) return api.sendMessage('[ بنك اكامي ] » رصيدك لا يكفي لإجراء هذه الصفقة!', threadID, messageID)
+    if(parseInt(money) < parseInt(moneyInput)) return api.sendMessage('[ بنك ميكو] » رصيدك لا يكفي لإجراء هذه الصفقة!', threadID, messageID)
       else {
         await await Currencies.increaseMoney(senderID, parseInt(moneyInput));
         userData.money = parseInt(money) - parseInt(moneyInput)
         writeFileSync(pathData, JSON.stringify(user, null, 2));
-        return api.sendMessage(`[ بنك اكامي ] »تم سحب  ${parseInt(moneyInput)}$ بنجاح \n الرصيد المتبقي هو ${userData.money}$`, threadID, messageID)
+        return api.sendMessage(`[ بنك ميكو] »تم سحب  ${parseInt(moneyInput)}$ بنجاح \n الرصيد المتبقي هو ${userData.money}$`, threadID, messageID)
       }
     }
   }
-  else return api.sendMessage(`=====🏦 بنك اكامي 🏦=====\n\n[بنك تسجيل] - سجل لإيداع الأموال في بنك اكامي💹\n[بنك عرض] - عرض المبلغ في بنك اكامي💳\n[بنك ايداع] - الإيداع في بنك اكامي💷\n[بنك سحب] - سحب الأموال من بنك اكامي\n\n💲 معدل الفائدة الحالي : +${laisuat}% في ${timeIM/60} هذه اللحظه`, threadID, messageID)
+  else return api.sendMessage(`=====🏦 بنك اكامي 🏦=====\n\n[بنك تسجيل]سجل لإيداع الأموال في بنك ميكو 💹\n[بنك عرض] - عرض المبلغ في بنك ميكو 💳\n[بنك ايداع] - الإيداع في بنك ميكو 💷\n[بنك سحب] - سحب الأموال من بنك ميكو \n\n💲 معدل الفائدة الحالي : +${laisuat}% في ${timeIM/60} هذه اللحظه`, threadID, messageID)
       }
